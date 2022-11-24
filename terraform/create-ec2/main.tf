b9a387382d1d0295dd2f0169d7c158bd6a271dc2
manjumh021 <81350837+manjumh021@users.noreply.github.com>
2022-11-24 11:51:51 +0530

    added security to group to main.tf

--- terraform/create-ec2/main.tf
+++ terraform/create-ec2/main.tf
@@ -13,10 +13,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "snipe-it_server" {
-  ami           = "ami-02ea247e531eb3ce6"
+  ami = "ami-02ea247e531eb3ce6"
   instance_type = "t2.micro"
-  key_name               = "aws-manju-key2"
-  vpc_security_group_ids = [sg-0b20fefb47205d6ba]
+  key_name = "aws-manju-key2"
+  vpc_security_group_ids = [aws_security_group.main.id]
   connection {
     type    = "ssh"
     host    = self.public_ip
@@ -26,4 +26,33 @@ resource "aws_instance" "snipe-it_server" {
   tags = {
     Name = "dev-environment"
   }
+}
+
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    }
+  ]
 }
