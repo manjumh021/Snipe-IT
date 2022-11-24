f1e769a65844286def99362abcefa88e5c1c4405
manjumh021 <81350837+manjumh021@users.noreply.github.com>
2022-11-24 06:34:18 +0000

    update main.tf

--- terraform/create-ec2/main.tf
+++ terraform/create-ec2/main.tf
@@ -46,13 +46,13 @@ resource "aws_security_group" "main" {
     {
       cidr_blocks      = ["0.0.0.0/0", ]
       description      = ""
-      from_port        = 0
+      from_port        = 22
       ipv6_cidr_blocks = []
       prefix_list_ids  = []
-      protocol         = "-1"
+      protocol         = "tcp"
       security_groups  = []
       self             = false
-      to_port          = 0
+      to_port          = 22
     }
   ]
 }
